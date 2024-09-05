8f53c041d0aab0de6807813200ddb990cb236f1a
Wanderson Mendonça <wandertake23@outlook.com>
2024-09-04 21:20:38 -0300

    JS e bg
    adicionei javascript e a foto da nba de bg

---
+++ Imersao/dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+    titulo : "Los Angeles Lakers",
+    descrição : "Los Angeles Lakers são a franquia mais bem-sucedida da história da NBA, com um impressionante total de 17 títulos conquistados. Essa marca histórica é fruto de uma combinação única de talento, tradição e paixão pelo basquete. Ao longo das décadas, os Lakers formaram diversas dinastias, com estrelas como Magic Johnson, Kareem Abdul-Jabbar, Kobe Bryant e LeBron James, que levaram a equipe ao topo do esporte. A rica história da franquia, marcada por rivalidades intensas e momentos inesquecíveis, consolidou os Lakers como um dos times mais populares e respeitados do mundo",
+    link : "https://pt.wikipedia.org/wiki/Los_Angeles_Lakers"
+},
+{
+    titulo : "Boston Celtics",
+    descrição : "Os Boston Celtics são uma das franquias mais antigas e tradicionais da NBA, fundada em 1946. Com 17 títulos de campeão, os Celtics possuem uma das maiores torcidas da liga. A equipe é conhecida por sua rivalidade histórica com os Los Angeles Lakers e por ter revelado grandes talentos como Bill Russell, Larry Bird e atualmente Jayson Tatum e Jaylen Brown. A rica história e a paixão dos torcedores fazem dos Celtics um dos times mais icônicos do basquete.",
+    link : "https://pt.wikipedia.org/wiki/Boston_Celtics"
+},
+{
+    titulo : "Dallas Mavericks",
+    descrição : "Os Dallas Mavericks, fundados em 1980, conquistaram seu primeiro título da NBA em 2011, com Dirk Nowitzki como principal estrela. A equipe, baseada em Dallas, Texas, tem se destacado nos últimos anos com uma jovem e talentosa equipe. A franquia é conhecida por sua paixão por desenvolver jovens talentos e por sua rivalidade com os San Antonio Spurs." ,
+    link : "https://pt.wikipedia.org/wiki/Dallas_Mavericks"
+}
+];
